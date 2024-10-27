99a1d3132557eae8b67ae29b84cc57336a8844fd
Tokumura <anton.tereshchenko.0003@bk.ru>
2024-10-27 03:44:00 +0300

    add id to user_reg

--- lib/src/user_register.dart
+++ lib/src/user_register.dart
@@ -1,17 +1,20 @@
 import 'package:equatable/equatable.dart';
 
 class UserRegister extends Equatable {
+  final String id;
   final String username;
   final String password_hash;
   final String secret_word;
 
   UserRegister({
+    required this.id,
     required this.username,
     required this.password_hash,
     required this.secret_word,
   });
   factory UserRegister.fromJson(Map<String, dynamic> json) {
     return UserRegister(
+      id: json['id'],
       username: json['username'],
       password_hash: json['password_hash'],
       secret_word: json['secret_word'],
@@ -19,11 +22,12 @@ class UserRegister extends Equatable {
   }
 
   Map<String, dynamic> toJson() => {
+        'id': id,
         'username': username,
         'password_hash': password_hash,
         'secret_word': secret_word
       };
 
   @override
-  List<Object?> get props => [username, password_hash, secret_word];
+  List<Object?> get props => [id, username, password_hash, secret_word];
 }
