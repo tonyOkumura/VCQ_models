6320b88425f400db707c0a4ebd152122e4612481
Tokumura <anton.tereshchenko.0003@bk.ru>
2024-10-27 03:14:11 +0300

    change password to password_hash

--- lib/src/user_login.dart
+++ lib/src/user_login.dart
@@ -2,27 +2,27 @@ import 'package:equatable/equatable.dart';
 
 class UserLogin extends Equatable {
   final String username;
-  final String password;
+  final String password_hash;
 
   UserLogin({
     required this.username,
-    required this.password,
+    required this.password_hash,
   });
   factory UserLogin.fromJson(Map<String, dynamic> json) {
     return UserLogin(
       username: json['username'],
-      password: json['password'],
+      password_hash: json['password_hash'],
     );
   }
 
   Map<String, dynamic> toJson() => {
         'username': username,
-        'password': password,
+        'password_hash': password_hash,
       };
 
   @override
   List<Object?> get props => [
         username,
-        password,
+        password_hash,
       ];
 }
--- lib/src/user_register.dart
+++ lib/src/user_register.dart
@@ -2,25 +2,28 @@ import 'package:equatable/equatable.dart';
 
 class UserRegister extends Equatable {
   final String username;
-  final String password;
+  final String password_hash;
   final String secret_word;
 
   UserRegister({
     required this.username,
-    required this.password,
+    required this.password_hash,
     required this.secret_word,
   });
   factory UserRegister.fromJson(Map<String, dynamic> json) {
     return UserRegister(
       username: json['username'],
-      password: json['password'],
+      password_hash: json['password_hash'],
       secret_word: json['secret_word'],
     );
   }
 
-  Map<String, dynamic> toJson() =>
-      {'username': username, 'password': password, 'secret_word': secret_word};
+  Map<String, dynamic> toJson() => {
+        'username': username,
+        'password_hash': password_hash,
+        'secret_word': secret_word
+      };
 
   @override
-  List<Object?> get props => [username, password, secret_word];
+  List<Object?> get props => [username, password_hash, secret_word];
 }
